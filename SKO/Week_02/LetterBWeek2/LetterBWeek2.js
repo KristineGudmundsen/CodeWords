2f94f70ba2452155edefeaac51c99b3a17a390eb
KristineGudmundsen <68985214+KristineGudmundsen@users.noreply.github.com>
2020-10-22 23:10:58 +1100

    Add files via upload

---
+++ SKO/Week_02/LetterBWeek2/LetterBWeek2.js
@@ -0,0 +1,16 @@
+//Creater unknown?
+//remixed by Kristine
+
+function setup() {
+createCanvas (500,500);
+}
+
+function draw() {
+background (mouseY/2,mouseY/2,mouseX/2);
+stroke (255);
+strokeWeight (mouseY/2,mouseX/2,mouseX/2);
+fill (mouseX);
+line (100,100,100,400);
+circle (175,175,150,150);
+circle (175,330,150,150);
+}
